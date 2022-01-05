f23d8112efe02d345abdba052c0d25ea7bbd60c1
TiCefops <SistemasCefops@2022>
2022-01-05 12:12:31 -0300

    dropdown com tipo de pagamento

---
+++ lib/app/views/adm/finances/interno/widgets/widget_dropmenu_type_entitity.dart
@@ -0,0 +1,47 @@
+import 'package:cefops/app/views/adm/finances/interno/controller/new_finance_controller.dart';
+import 'package:cefops/shared/themes/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+///
+class DropdownmenuTypeEntity extends StatelessWidget {
+  ///
+  const DropdownmenuTypeEntity({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    NewFinanceInternalController _controller=Get.find<NewFinanceInternalController>();
+
+    return Obx(
+            () {
+          return DropdownButton<String>(
+            value: _controller.formaPagamento.value,
+            icon:  Icon(Icons.arrow_downward,color: AppColors.orange,),
+            iconSize: 24,
+            elevation: 16,
+            style: const TextStyle(
+                color: Colors.black
+            ),
+            underline: Container(
+              height: 2,
+              color: Colors.blueAccent,
+            ),
+            onChanged: (String? newValue) {
+
+              _controller.formaPagamento.value = newValue!;
+
+
+            },
+            items: <String>['Dinheiro','Cartão','PIX','Boleto']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            })
+                .toList(),
+          );
+        }
+    );
+  }
+}
