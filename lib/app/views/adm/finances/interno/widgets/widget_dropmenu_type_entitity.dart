3ce2c522d007db024c44b73ca28bef4eff457caa
TiCefops <SistemasCefops@2022>
2022-01-05 12:18:10 -0300

    coreção do bug por falta do get.find

--- lib/app/views/adm/finances/interno/widgets/widget_dropmenu_type_entitity.dart
+++ lib/app/views/adm/finances/interno/widgets/widget_dropmenu_type_entitity.dart
@@ -2,7 +2,7 @@ import 'package:cefops/app/views/adm/finances/interno/controller/new_finance_con
 import 'package:cefops/shared/themes/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
+
 ///
 class DropdownmenuTypeEntity extends StatelessWidget {
   ///
@@ -10,38 +10,34 @@ class DropdownmenuTypeEntity extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    NewFinanceInternalController _controller=Get.find<NewFinanceInternalController>();
+    NewFinanceInternalController _controller =
+        Get.find<NewFinanceInternalController>();
 
-    return Obx(
-            () {
-          return DropdownButton<String>(
-            value: _controller.formaPagamento.value,
-            icon:  Icon(Icons.arrow_downward,color: AppColors.orange,),
-            iconSize: 24,
-            elevation: 16,
-            style: const TextStyle(
-                color: Colors.black
-            ),
-            underline: Container(
-              height: 2,
-              color: Colors.blueAccent,
-            ),
-            onChanged: (String? newValue) {
-
-              _controller.formaPagamento.value = newValue!;
-
-
-            },
-            items: <String>['Dinheiro','Cartão','PIX','Boleto']
-                .map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            })
-                .toList(),
+    return Obx(() {
+      return DropdownButton<String>(
+        value: _controller.formaPagamento.value,
+        icon: Icon(
+          Icons.arrow_downward,
+          color: AppColors.orange,
+        ),
+        iconSize: 24,
+        elevation: 16,
+        style: const TextStyle(color: Colors.black),
+        underline: Container(
+          height: 2,
+          color: Colors.blueAccent,
+        ),
+        onChanged: (String? newValue) {
+          _controller.formaPagamento.value = newValue!;
+        },
+        items: <String>['Dinheiro', 'Cartão', 'PIX', 'Boleto']
+            .map<DropdownMenuItem<String>>((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value),
           );
-        }
-    );
+        }).toList(),
+      );
+    });
   }
 }
